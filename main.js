46f3f09702a201dcf8c4677444dc47359f9296ec
Maksym Zhuk <bitmaksum550@gmail.com>
2024-06-17 13:46:02 +0300

    add filter on characters page

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ $(`#watchList`).click(() => {
     })
 });
 
-$(`#logo`).click(()=>{
+$(`#logo`).click(() => {
     $('#characters').css({
         'color': `#fff`,
         'text-shadow': `none`
@@ -103,20 +103,42 @@ let pageOpen = 1;
 let numberPage;
 function getCharacters(page = 1) {
     axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`)
-        .then(res => {
-            $(`.charactersContainer`).empty();
-            for (let el of res.data.results) {
-                $(`.charactersContainer`).append(`
-                <div class="characterItem">
-                    <img src="${el.image}" alt="">
-                    <h3>${el.name}</h3>
-                    <button id="code${el.id}" class="viewInfoBtn">View</button>
-                </div>
-                `)
+    .then(res => {
+        $('.charactersContainer').empty();
+        for (let el of res.data.results) {
+            if ((!$('#species').val() || el.species === $('#species').val()) &&
+                (!$('#status').val() || el.status === $('#status').val()) &&
+                (!$('#gender').val() || el.gender === $('#gender').val())) {
+                $('.charactersContainer').append(`
+                    <div class="characterItem">
+                        <img src="${el.image}" alt="">
+                        <h3>${el.name}</h3>
+                        <button id="code${el.id}" class="viewInfoBtn">View</button>
+                    </div>
+                `);
             }
-            numberPage = res.data.info.pages
-        });
+        }
+
+        numberPage = res.data.info.pages;
+    })
 }
+
+getCharacters(pageOpen)
+
+$('#species, #status, #gender').change(() => {
+    pageOpen = 1
+    getCharacters(pageOpen)
+    $(`#numberPage`).val(pageOpen);
+});
+
+$(`#reset`).click(()=>{
+    $('#species').val(``);
+    $('#status').val(``);
+    $('#gender').val(``);
+    pageOpen = 1
+    getCharacters(pageOpen)
+    $(`#numberPage`).val(pageOpen);
+});
 
 function getCharacterInfo(id) {
     axios.get(`https://rickandmortyapi.com/api/character/${id}`)
@@ -124,15 +146,18 @@ function getCharacterInfo(id) {
             $(`.rowContainer`).empty();
             $(`.rowContainer`).append(`
                 <h3>${res.data.name}</h3>
-                <p>${res.data.gender}</p>
-                <p>${res.data.status}</p>
-                <p>${res.data.species}</p>
+                <p>Location: ${res.data.location.name}</p>
+                <p>Gender: ${res.data.gender}</p>
+                <p>Status: ${res.data.status}</p>
+                <p>Species: ${res.data.species}</p>
             `)
         });
 }
 
+
+
 $(`#numberPage`).val(pageOpen);
-getCharacters(pageOpen);
+
 
 $(`.wrap`).click((e) => {
     if ((e.target.id).substring(0, 4) == `code`) {
